aec3e7c9232748329f9a2daf186b31b831022465
Dewayne Allen <dewayneallen76@gmail.com>
2017-02-02 14:02:04 -0600

    SET TASKS IN MODEL

--- database/migrations/2017_02_02_195102_create_tasks_table.php
+++ database/migrations/2017_02_02_195102_create_tasks_table.php
@@ -14,6 +14,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('body');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
