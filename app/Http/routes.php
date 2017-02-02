3f2699192897113d1c90a70b27ac528fbf1efac3
Dewayne Allen <dewayneallen76@gmail.com>
2017-02-02 15:26:11 -0600

    found typo in route causing data not to load on ajax request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,10 @@
 |
 */
 
+Route::get('api/tasks', function() {
+	return App\Task::latest()->get();
+});
+
 Route::get('/', function () {
-	$tasks = App\Task::latest()->get();
-    return view('welcome', compact('tasks'));
+    return view('welcome');
 });
